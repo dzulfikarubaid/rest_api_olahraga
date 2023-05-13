7fb53681c200147abbb0de557a09c63529eb3f83
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-05-13 20:37:21 +0700

    add methods

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from knox import views as knox_views
+
 
 urlpatterns = [
-    path('', views.index), #https://api-servis-mobil.vercel.app
+    path('', views.index),
     path('add/', views.Add, name='add'),
     path('list/',views.List, name='list'),
     path('update/<int:id>/',views.Update, name='update'),
